866ac5bfda9fe97dc03bad6b5d53b2d919f0e954
domenicocandido <candidodomenico87@gmail.com>
2025-12-09 20:29:09 +0100

    update

---
+++ Esame/JavaScript/Lezione1/Esercizi/script.js
@@ -0,0 +1,17 @@
+function mostraAlert() {
+  alert("Ciao! Questo è il mio primo alert!");
+}
+
+function scriviConsole() {
+  console.log("Questo è il mio primo console.log!");
+}
+
+function cambiaTitolo(){
+    document.getElementById("titolo").textContent = "Titolo modificato con JavaScript";
+}
+
+function richiesta(){
+    console.log("Bottone cliccato!");
+    document.getElementById("paragrafo").style.color= "red";
+
+}
---
+++ Esame/JavaScript/Lezione1/script.js
@@ -0,0 +1,12 @@
+function mostraAlert() {
+  alert("Ciao! Questo è il mio primo alert!");
+}
+
+function scriviConsole() {
+  console.log("Questo è il mio primo console.log!");
+}
+
+function cambiaTesto() {
+  document.getElementById("titolo").textContent =
+    "Testo cambiato tramite JavaScript!";
+}
